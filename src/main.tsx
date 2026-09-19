f95f0f519d7a85c5bdaace820f98a0329a480153
Marco <marco.dongu@gmail.com>
2026-09-19 10:05:03 +0200

    P330

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Icone e font BUNDLATI in locale (niente CDN → CSP default-src 'self', offline-ok)
+import '@tabler/icons-webfont/dist/tabler-icons.min.css'
+import '@fontsource/jetbrains-mono/400.css'
+import '@fontsource/jetbrains-mono/500.css'
+import '@fontsource/ibm-plex-sans/400.css'
+import '@fontsource/ibm-plex-sans/500.css'
+import '@fontsource/ibm-plex-sans/600.css'
 import './index.css'
 import App from './App.tsx'
 
